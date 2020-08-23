d19a27141b3818d3e0cb108549e153ddcca19272
510988896@qq.com <FANbin8807915>
2020-08-23 23:44:23 +0800

    springcache

--- spring-boot-redis/src/main/java/com/neo/RedisApplication.java
+++ spring-boot-redis/src/main/java/com/neo/RedisApplication.java
@@ -2,6 +2,8 @@ package com.neo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
 
 @SpringBootApplication
 public class RedisApplication {
