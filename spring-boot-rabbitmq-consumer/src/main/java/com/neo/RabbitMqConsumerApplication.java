7075b60a26040b5ddad862acc8cc8857d41980a3
510988896@qq.com <FANbin8807915>
2021-03-11 20:53:14 +0800

    rabbitmq

---
+++ spring-boot-rabbitmq-consumer/src/main/java/com/neo/RabbitMqConsumerApplication.java
@@ -0,0 +1,1 @@
+package com.neo;import org.springframework.boot.SpringApplication;import org.springframework.boot.autoconfigure.SpringBootApplication;/** * Created by cainiao on 2021/3/7 11:26 上午 */@SpringBootApplicationpublic class RabbitMqConsumerApplication {    public static void main(String[] args) {        SpringApplication.run(RabbitMqConsumerApplication.class,args);    }}
